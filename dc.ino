3c559910722cd7706330d4fabb3aad2fea2de423
Wyatt Cook <wyattcook2018@u.northwestern.edu>
2016-03-10 15:53:23 -0600

    Fixed slight potentiometer bug and added second button and led

--- dc.ino
+++ dc.ino
@@ -4,22 +4,27 @@
   int pressed = 0;
   int brightness =0;
   int potPin =2;
+  int secondpressed = 1;
+  int secondbutton =6;
+  int secondledPin = 5;
 void setup() {
   // put your setup code here, to run once:
  pinMode(ledPin, OUTPUT);
  pinMode(button, INPUT);
- //pinMode(potPin, INPUT);
+ pinMode(secondledPin, OUTPUT);
+ pinMode(secondbutton, OUTPUT);
  Serial.begin(9600);
 }
 
 void loop() { 
   // put your main code here, to run repeatedly:
   pressed = digitalRead(button);
+  secondpressed = digitalRead(secondbutton);
   if (pressed == HIGH){
 
     brightness = analogRead(potPin);
     brightness = brightness/2;
-    brightness=brightness-256;
+    brightness=brightness-255;
     Serial.print(brightness);
     Serial.print( "\n");
     analogWrite(ledPin, brightness);
@@ -29,6 +34,13 @@ else {
     digitalWrite(ledPin, LOW);
 
 }
+  if (secondpressed == HIGH) {
+    digitalWrite(secondledPin, HIGH);
+  }
+  else {
+    digitalWrite(secondledPin, LOW);
+  }
+  
   
 
 }
